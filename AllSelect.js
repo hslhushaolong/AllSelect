d41db1023261f6e4f1714cb76485b35000f72356
duyanpeng <215028726@qq.com>
2017-11-30 13:52:55 +0800

    支持较多属性

--- AllSelect.js
+++ AllSelect.js
@@ -17,25 +17,40 @@ class AllSelect extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: this.props.value || [],               // 选中的value数组
+      value: this.props.value || props.defaultValue || [],               // 选中的value数组
       options: props.children || [],               // option选项
       getAllSelectValue: props.onChange,           // 获得value值
       style: props.style || {},                    // style
       placeholder: props.placeholder || '',        // placeholder
+
       showSearch: props.showSearch || true,        // 是否可以输入值搜索
       allowClear: props.allowClear || true,        // 是否展示删除按钮
+      defaultActiveFirstOption: props.defaultActiveFirstOption || true,
+      disabled: props.disabled || false,
+      labelInValue: props.labelInValue || false,
+      notFoundContent: props.notFoundContent || 'Not Found',
+      filterOption: props.filterOption || true,
+      onSearch: props.onSearch,
+
     };
   }
 
   componentWillReceiveProps = (nextProps) => {
     this.setState({
-      value: nextProps.value || [],
+      value: nextProps.value || nextProps.defaultValue || [],
       options: nextProps.children || [],
       getAllSelectValue: nextProps.onChange,
       style: nextProps.style || {},
       placeholder: nextProps.placeholder || '',
-      showSearch: nextProps.showSearch || true,        // 是否可以输入值搜索
-      allowClear: nextProps.allowClear || true,        // 是否展示删除按钮
+
+      showSearch: nextProps.showSearch || true,
+      allowClear: nextProps.allowClear || true,
+      defaultActiveFirstOption: nextProps.defaultActiveFirstOption || true,
+      disabled: nextProps.disabled || false,
+      labelInValue: nextProps.labelInValue || false,
+      notFoundContent: nextProps.notFoundContent || 'Not Found',
+      filterOption: nextProps.filterOption || true,
+      onSearch: nextProps.onSearch
     });
   }
 
@@ -45,6 +60,13 @@ class AllSelect extends React.Component {
       return true;
     } else {
       return false;
+    }
+  }
+
+  // onSearch回调
+  onSearch = (value) => {
+    if (this.exist(this.state.onSearch)) {
+      this.state.onSearch(value)
     }
   }
 
@@ -92,10 +114,12 @@ class AllSelect extends React.Component {
   }
 
   render() {
-    const { options, style, value, placeholder, defaultValue, allowClear, showSearch } = this.state;
+    const { options, style, value, placeholder, defaultValue, allowClear, showSearch, defaultActiveFirstOption,
+      disabled, labelInValue, notFoundContent, filterOption
+     } = this.state;
     return (
       <Select
-        className="maxHeight"              // 限制高度不超过四行选项的class类
+        className="maxHeight"               // 限制高度不超过四行选项的class类
         showSearch={showSearch}             // 可以输入值搜索
         optionFilterProp="children"         // 搜索内容在option范围内
         style={style}
@@ -104,6 +128,12 @@ class AllSelect extends React.Component {
         value={value}
         onChange={this.handleChange}
         placeholder={placeholder}
+        defaultActiveFirstOption={defaultActiveFirstOption}
+        disabled={disabled}
+        labelInValue={labelInValue}
+        notFoundContent={notFoundContent}
+        filterOption={filterOption}
+        onSearch={this.onSearch}
       >
         {this.arrayConverson(options)}
       </Select>
