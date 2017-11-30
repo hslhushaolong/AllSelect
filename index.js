5dc33ab4a9f54a2fb907d3fd15d4e6fcb5f6b2c0
duyanpeng <215028726@qq.com>
2017-11-30 10:56:11 +0800

    整理

--- index.js
+++ index.js
@@ -25,38 +25,64 @@ class App extends React.Component {
     })
 
   }
-  onChange = () => {
+  // 获取
+  getForm = () => {
     const file = this.props.form.getFieldsValue()
     console.log(file,'file')
   }
-  onChange2 = () => {
+  // 清除
+  cleForm = () => {
     this.props.form.resetFields()
-    this.setState({
-      userName:[]
-    })
     const file = this.props.form.getFieldsValue()
     console.log(file,'form')
   }
-  onChange2 = () => {
-    this.props.form.resetFields()
-    this.setState({
-      userName:[]
-    })
-    const file = this.props.form.getFieldsValue()
-    console.log(file,'form')
-  }
-  onChange3 = () => {
+
+// 设置
+  setForm = () => {
     this.props.form.setFields({userName:{value:['2','4','5']}})
   }
+  //组件获取
+  getUserName = () => {
+    console.log(this.state.userName,'com')
+  }
+  // 组件清除
+  cleUserName = () => {
+    this.setState({
+      userName:[],
+    })
+  }
+  // 组件设置
+  setUserName = () => {
+    this.setState({
+      userName:['2','4','5']
+    })
+  }
+
   render() {
     return (
-      <div>
+      <div style={{textAlign: 'center'}}>
+        <AllSelect
+          placeholder="我没有在表单里"
+          onChange={this.handleChange}
+          style={{width:'30%'}}
+          value={this.state.userName}
+          >
+            {this.state.array.map(item=>(
+              <Option value={item.value} key={item.value}>{item.name}</Option>
+            ))}
+        </AllSelect>
+        <br/>
+        <Button style={{marginTop: '30px'}} onClick={this.getUserName}>获取表单值com</Button><br/>
+        <Button onClick={this.cleUserName}>清空表单值com</Button><br/>
+        <Button style={{marginBottom: '30px'}} onClick={this.setUserName}>设置表单值com</Button>
       <Form>
         <FormItem>
-          {this.props.form.getFieldDecorator('userName',{ initialValue: [] })(
+          {this.props.form.getFieldDecorator('userName'
+          // ,{ initialValue: [] }
+        )(
             <AllSelect
-              onChange={this.handleChange}
-              style={{width:'30%'}}
+              placeholder="我在表单里"
+              style={{width:'30%',margin: '0 auto'}}
               >
                 {this.state.array.map(item=>(
                   <Option value={item.value} key={item.value}>{item.name}</Option>
@@ -65,9 +91,9 @@ class App extends React.Component {
           )}
         </FormItem>
       </Form>
-      <Button onClick={this.onChange}>获取表单值form</Button><br/>
-      <Button onClick={this.onChange2}>清空表单值form</Button><br/>
-      <Button onClick={this.onChange3}>设置表单值form</Button>
+      <Button onClick={this.getForm}>获取表单值form</Button><br/>
+      <Button onClick={this.cleForm}>清空表单值form</Button><br/>
+      <Button onClick={this.setForm}>设置表单值form</Button>
       </div>
     );
   }
